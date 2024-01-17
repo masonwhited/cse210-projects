d9bae5cd9b0c71e495c404fef0468f95e5037792
whitedmason <masonwhited@byui.edu>
2024-01-17 15:37:41 -0700

    Prep 4 changes

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int n;
+        do
+        {
+            Console.WriteLine("Please enter a number: ");
+            n = int.Parse(Console.ReadLine());
+            numbers.Add(n);
+        }
+        while (n != 0);
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            n += numbers[i];
+        }
+        Console.WriteLine($"Sum = {n}.");
     }
 }
