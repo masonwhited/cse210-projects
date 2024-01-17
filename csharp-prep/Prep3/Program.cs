19113d5797e193f1b5c6d8d36a15b210bf3fe0c1
whitedmason <masonwhited@byui.edu>
2024-01-17 10:49:11 -0700

    prep 3 update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 11);
+        int number = randomGenerator.Next(1, 100);
         int guess;
         do
         {
