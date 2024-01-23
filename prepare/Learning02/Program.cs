4a0ff66f3e68889ff133f3adb713fbc168b4d40a
whitedmason <masonwhited@byui.edu>
2024-01-23 16:39:13 -0700

    maybe fixed?

---
+++ prepare/Learning02/Program.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var jobs = new List<Job>();
+
+        var job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        jobs.Add(job1);
+
+        job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        jobs.Add(job1);
+
+        foreach (var j in jobs)
+        {
+            // System.Console.WriteLine($"{c.make} {c.model}: totalRange = {c.TotalRange()}");
+            j.Display();
+        }
+    }
+}
