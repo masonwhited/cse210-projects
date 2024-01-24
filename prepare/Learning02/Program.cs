a39a8d22c87e3a3d0058e4bcef4c1d2286467851
whitedmason <masonwhited@byui.edu>
2024-01-23 17:37:57 -0700

    Learning02 finished?

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,27 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        var jobs = new List<Job>();
-
-        var job1 = new Job();
+        Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
         job1._startYear = 2020;
         job1._endYear = 2022;
 
-        jobs.Add(job1);
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
 
-        job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Apple";
-        job1._startYear = 2022;
-        job1._endYear = 2024;
+        Resume myResume = new Resume();
+        myResume._name = "Mason Whited";
 
-        jobs.Add(job1);
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
 
-        foreach (var j in jobs)
-        {
-            j.Display();
-        }
+        myResume.Display();
     }
 }
