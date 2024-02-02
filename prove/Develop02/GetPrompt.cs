c3de3e68bf76104491be54d7d725c2d8c0d3c23b
whitedmason <masonwhited@byui.edu>
2024-02-02 09:09:01 -0700

    new class

---
+++ prove/Develop02/GetPrompt.cs
@@ -0,0 +1,15 @@
+public class GetPrompt
+{
+    public string Prompt()
+    {
+        string[] prompts = new string[5];
+        prompts[0] = "Who was the most interesting person I interacted with today?";
+        prompts[1] = "What was the best part of my day?";
+        prompts[2] = "How did I see the hand of the Lord in my life today?";
+        prompts[3] = "What was the strongest emotion I felt today?";
+        prompts[4] = "If I had one thing I could do over today, what would it be?";
+        Random random = new Random();
+        int randomNumber = random.Next(0, 5);
+        return prompts[randomNumber];
+    }
+}
