b3b8b9dc38c0180a943585a86f4548cdfc202feb
whitedmason <masonwhited@byui.edu>
2024-02-05 10:58:39 -0700

    Add Fraction class with constructors.

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -1,4 +1,26 @@
+using System.Globalization;
+
 public class Fraction
 {
-
+    private int topNumber;
+    private int bottomNumber;
+    // Constructors
+    public Fraction()
+    {
+        topNumber = 1;
+        bottomNumber = 1;
+        Console.WriteLine($"{topNumber}/{bottomNumber}");
+    }
+    public Fraction(int num)
+    {
+        topNumber = num;
+        bottomNumber = 1;
+        Console.WriteLine($"{topNumber}/{bottomNumber}");
+    }
+    public Fraction(int top, int bottom)
+    {
+        topNumber = top;
+        bottomNumber = bottom;
+        Console.WriteLine($"{topNumber}/{bottomNumber}");
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
     }
 }
