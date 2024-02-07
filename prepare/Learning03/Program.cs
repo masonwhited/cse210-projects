b05efb9a4344bf16acf3b65e8db74436ce1e6f60
whitedmason <masonwhited@byui.edu>
2024-02-07 08:47:39 -0700

    Update Fraction class to use properties for GetFractionString and GetDecimalValue methods

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -6,19 +6,27 @@ class Program
     {
         // Console.WriteLine("Hello Learning03 World!");
         Fraction first = new Fraction();
-        Console.WriteLine(first.GetFractionString());
-        Console.WriteLine(first.GetDecimalValue());
+        var set1 = first.GetFractionString();
+        Console.WriteLine(set1);
+        var get1 = first.GetDecimalValue();
+        Console.WriteLine(get1);
 
         Fraction second = new Fraction(5);
-        Console.WriteLine(second.GetFractionString());
-        Console.WriteLine(second.GetDecimalValue());
+        var set2 = second.GetFractionString();
+        Console.WriteLine(set2);
+        var get2 = second.GetDecimalValue();
+        Console.WriteLine(get2);
 
         Fraction third = new Fraction(3, 4);
-        Console.WriteLine(third.GetFractionString());
-        Console.WriteLine(third.GetDecimalValue());
+        var set3 = third.GetFractionString();
+        Console.WriteLine(set3);
+        var get3 = third.GetDecimalValue();
+        Console.WriteLine(get3);
 
         Fraction fourth = new Fraction(1, 3);
-        Console.WriteLine(fourth.GetFractionString());
-        Console.WriteLine(fourth.GetDecimalValue());
+        var set4 = fourth.GetFractionString();
+        Console.WriteLine(set4);
+        var get4 = fourth.GetDecimalValue();
+        Console.WriteLine(get4);
     }
 }
