bc33a530864bd32ba52c5fdb62ec31ae884eeb1b
whitedmason <masonwhited@byui.edu>
2024-02-13 13:21:59 -0700

    commented the Word Class

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,19 +1,25 @@
 using Microsoft.VisualBasic;
 
-public class Word {
-
+public class Word
+{
+    //creates a variable to hold the string word
     public string word;
 
+    // Replaces all non-punctuation characters in the current word with the underscore character ("_") to hide the word.
     public void Hide()
     {
         string blank = "";
         foreach (char a in word)
         {
+            // Check if the character is punctuation
             if (!char.IsPunctuation(a))
-                {blank += "_";}
+                // If it's not punctuation, replace it with an underscore character
+                blank += "_";
             else
-                {blank += a;}
+                // If it's punctuation, keep the character as is
+                blank += a;
         }
+        // Set the current word to the new hidden string
         word = blank;
     }
 
