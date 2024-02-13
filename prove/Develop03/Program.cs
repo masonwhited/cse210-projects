5f339d2398f0ec987c367480c8458247b955c9ce
whitedmason <masonwhited@byui.edu>
2024-02-13 12:02:04 -0700

    Refactored Scripture and Reference classes. And added comments

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,33 +6,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        //Console.WriteLine("Hello Develop03 World!");
+
+        // Creates an instance of the Scripture class
         Scripture scripture = new Scripture();
 
+        //creates a variable to hold the string scripture
         string script = "John 3:16 For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.";
 
         //string script = "Alma 12:27 But behold, it was not so; but it was appointed unto men that they must die; and after death, they must come to judgment, even that same judgment of which we have spoken, which is the end.";
-        
+
+        //passes the variable script into the variable scripture in the Scripture class
         scripture.scripture = script;
 
+        //clears the terminal
         Console.Clear();
+        //calls the DisplayReference function from the Scripture Class
         scripture.DisplayReference();
+        //calls the DisplayVerses function from the Scripture Class
         scripture.DisplayVerses();
 
+        //prompts the user to press the enter key or type out the word 'quit'
         Console.WriteLine("\n\nPress enter to continue or type 'quit' to finish: ");
+        //reads the response and passes it into a variable
         string response = Console.ReadLine();
+        //create a while loop
         while (response != "quit")
         {
+            //clears the terminal
             Console.Clear();
+            //calls the DisplayReference function from the Scripture Class
             scripture.DisplayReference();
+            //calls the DisplayHide function from the Scripture Class
             scripture.DisplayHide();
 
             Console.WriteLine("\n\nPress enter to continue or type 'quit' to finish: ");
             response = Console.ReadLine();
 
-            if (scripture.wordCount.Count() == 0) { response = "quit";}            
+            //create an if statement stating that if wordCount == 0 then response = "quit"
+            if (scripture.wordCount.Count() == 0) { response = "quit"; }
         }
 
-        
+
     }
 }
--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,41 +1,53 @@
 using System;
-public class Reference {
-
+public class Reference
+{
+    //create a series of private variables to be referenced
     private string book;
     private int chap;
     private int startVerse;
     private int endVerse;
+    //create a BuildReference method that will build and return the object with multiple verses.
     public string BuildReference(string book, int chap, int startVerse, int endVerse)
     {
         return $"{book} {chap}:{startVerse}-{endVerse}";
     }
-
+    //create a BuildReference method that will build and return the object with a single verse.
     public string BuildReference(string book, int chap, int startVerse)
     {
         return $"{book} {chap}:{startVerse}";
     }
 
+    //create a BuildReference method for creating the scriptural reference.
     public void BuildReference(string scripture)
     {
+        //converts scripture into a list seperated by spaces
         var parts = scripture.Split(" ");
-        this.book = parts[0];
+        //stores the first element of the list into a variable
+        book = parts[0];
+        //stores the second element of the list into a variable
         string location = parts[1];
+        //seperates out the chapter and verses from the location and stores it in the parts variable
         parts = location.Split(":");
-        this.chap = int.Parse(parts[0]);
+        //stores the first element as an int in the chap variable
+        chap = int.Parse(parts[0]);
+        //stores the second element in the verses variable
         string verses = parts[1];
 
+        //checks if there is more than one verse and assigns them accordingly
         if (verses.Contains("-"))
         {
             parts = verses.Split("-");
-            this.startVerse = int.Parse(parts[0]);
-            this.endVerse = int.Parse(parts[1]);
+            startVerse = int.Parse(parts[0]);
+            endVerse = int.Parse(parts[1]);
         }
+        //if there is no "-" then only one verse was given so store it directly in the startVerse variable
         else
         {
-            this.startVerse = int.Parse(parts[1]);
+            startVerse = int.Parse(parts[1]);
         }
     }
 
+    //creates a ReturnReference method that will return the scriptural reference as a string.
     public string ReturnReference()
     {
         if (endVerse == 0)
@@ -47,7 +59,4 @@ public class Reference {
             return $"{book} {chap}:{startVerse}-{endVerse} ";
         }
     }
-
-
-
 }
