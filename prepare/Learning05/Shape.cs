ad89f6cd90bfee32eda003354bff880010ce5f1f
whitedmason <masonwhited@byui.edu>
2024-03-05 12:45:36 -0700

    created the shape class

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,17 @@
+public class Shape
+{
+    private string color;
+    public string GetColor()
+    {
+        return color;
+    }
+
+    public void SetColor(string color)
+    {
+        this.color = color;
+    }
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
