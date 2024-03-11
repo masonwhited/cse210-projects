8f004c87dd6d7cc3f4b6042352d29713c474e076
whitedmason <masonwhited@byui.edu>
2024-03-11 10:47:24 -0600

    Added virtual methods for exporting, creating, displaying, and updating goals.

--- prove/Develop05/Checklist.cs
+++ prove/Develop05/Checklist.cs
@@ -3,4 +3,21 @@ class Checklist : Goal
     private int progress;
     private int bonus;
     private int endTotal;
+
+    public virtual string Export()
+    {
+        return "";
+    }
+    public virtual string CreateGoal()
+    {
+        return "";
+    }
+    public virtual void Display()
+    {
+
+    }
+    public virtual void UpdateGoal()
+    {
+
+    }
 }
