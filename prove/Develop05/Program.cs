2756227bc23e9b72781f8005cfc49069a0a4536e
whitedmason <masonwhited@byui.edu>
2024-03-11 11:00:52 -0600

    Added a menu-driven interface to the program.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,10 +1,53 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         /* Console.WriteLine("Hello Develop05 World!"); */
+        bool keepGoing = true;
 
+        while (keepGoing)
+        {
+            Console.Clear();
+            var selection = ShowMenu();
+
+            if (selection == 1)
+            {
+
+            }
+            else if (selection == 2)
+            {
+
+            }
+            else if (selection == 3)
+            {
+
+            }
+            else if (selection == 4)
+            {
+
+            }
+            else if (selection == 5)
+            {
+
+            }
+            else if (selection == 6)
+            {
+                Console.Clear();
+                keepGoing = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Selection\nPlease try again.");
+            }
+        }
+        static int ShowMenu()
+        {
+            Console.WriteLine("Select Option: \n______________ \n 1. Create New Goal \n 2. List Goals \n 3. Save Goals \n 4. Load Goals \n 5. Record Event \n 6. Quit");
+            string input = Console.ReadLine();
+            return int.Parse(input);
+        }
     }
 }
